3fae75d512ed9b4250766eb8ebba51cc6ef442bf
x <mailbox_1729@hotmail.com>
2017-02-03 14:20:50 +0900

    Update comments in PerfectMatching_test.rb
    Translated some comments and added descriptions.

--- PerfectMatching_test.rb
+++ PerfectMatching_test.rb
@@ -1,7 +1,8 @@
 require_relative "PerfectMatching"
 require_relative "Graph"
 
-#口、日、目…フィボナッチ数列になる
+#Ladder type diagram.
+#One can easily see that the results coincide with the Fibonacci sequence.
 p PerfectMatching(SquareLattice(1,1)) #=> 2
 p PerfectMatching(SquareLattice(1,2)) #=> 3
 p PerfectMatching(SquareLattice(1,3)) #=> 5
@@ -9,13 +10,16 @@ p PerfectMatching(SquareLattice(1,4)) #=> 8
 p PerfectMatching(SquareLattice(1,5)) #=> 13
 p PerfectMatching(SquareLattice(1,6)) #=> 21
 
+#Formula for the number of perfect matching on Aztec Diamond
+#  #PM(AD(n))=2^(n(n+1)/2)
+#can be derived theoretically.
 
 p PerfectMatching(AztecDiamond(1)) #=> 2=2^1
 p PerfectMatching(AztecDiamond(2)) #=> 8=2^3
 p PerfectMatching(AztecDiamond(3)) #=> 64=2^6
+#Larger cases take some time..
 #p PerfectMatching(AztecDiamond(4)) #=> 1024=2^10
 #p PerfectMatching(AztecDiamond(5)) #=> 32768=2^15
-#2^(n(n+1)/2)
 
 
 
