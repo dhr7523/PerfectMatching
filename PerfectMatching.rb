f74652aea4dab935625692b391b75b9ab8ad64b8
sdhr <mailbox_1729@hotmail.com>
2017-02-10 02:08:10 +0900

    Fixed timing of duplication of graph

--- PerfectMatching.rb
+++ PerfectMatching.rb
@@ -1,7 +1,9 @@
 #counts perfect matching recursively.
 #weight of all edges are 1.
 
-def PerfectMatching(graph)
+def PerfectMatching(graph_)
+	graph=Marshal.load(Marshal.dump(graph_))
+
 	if graph.size==0 then
 		1
 	else
@@ -11,35 +13,34 @@ def PerfectMatching(graph)
 		#case in which the edge is not used for matching.
 		#the edge should not be connected with endpoints.
 		if graph.flatten(1).include?edge[0] and graph.flatten(1).include?edge[1]  then
-			num+=PerfectMatching(Marshal.load(Marshal.dump(graph)))
+			num+=PerfectMatching(graph)
 		end
 
 		#case in which the edge is used for matching.
 		#edges which is connected with the edge should not be connected with endpoints.
 		flag=true # false when an edge which is connected with the edge is connected with an endpoint.
 
-		newG=Marshal.load(Marshal.dump(graph))
-		for e in 0..newG.size-1 do
-			if newG[e].include?(edge[0]) then
-				deletedEdge=newG[e]
-				newG[e]=nil
-				if !newG.flatten(1).include?(deletedEdge[1-deletedEdge.index(edge[0])]) then
+		for e in 0..graph.size-1 do
+			if graph[e].include?(edge[0]) then
+				deletedEdge=graph[e]
+				graph[e]=nil
+				if !graph.flatten(1).include?(deletedEdge[1-deletedEdge.index(edge[0])]) then
 					flag=false
 					break
 				end
-			elsif newG[e].include?(edge[1]) then
-				deletedEdge=newG[e]
-				newG[e]=nil
-				if !newG.flatten(1).include?(deletedEdge[1-deletedEdge.index(edge[1])]) then
+			elsif graph[e].include?(edge[1]) then
+				deletedEdge=graph[e]
+				graph[e]=nil
+				if !graph.flatten(1).include?(deletedEdge[1-deletedEdge.index(edge[1])]) then
 					flag=false
 					break
 				end
 			end
 		end
-		newG.compact!
+		graph.compact!
 
 		if flag then
-			num+=PerfectMatching(newG)
+			num+=PerfectMatching(graph)
 		end
 		return num
 	end
